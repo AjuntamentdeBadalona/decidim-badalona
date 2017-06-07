456e71647da2046881cd80c489857290eaffea43
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-06-07 14:14:57 +0200

    Initial commit

---
+++ db/migrate/20170607072904_add_deleted_fields_to_users.decidim.rb
@@ -0,0 +1,7 @@
+# This migration comes from decidim (originally 20170605140421)
+class AddDeletedFieldsToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :decidim_users, :delete_reason, :text
+    add_column :decidim_users, :deleted_at, :datetime
+  end
+end
